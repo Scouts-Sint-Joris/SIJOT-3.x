1425119335a8987f1eb8700546a3e7e9d7618e2a
Tjoosten <Tjoosten3@gmail.com>
2017-05-15 19:12:17 +0200

    Fix mass-assign themes model

--- app/Themes.php
+++ app/Themes.php
@@ -4,7 +4,17 @@ namespace Sijot;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Themes
+ *
+ * @package Sijot
+ */
 class Themes extends Model
 {
-    //
+    /**
+     * Mass-assign fields for the database table.
+     *
+     * @var array
+     */
+    protected $fillable = [];
 }
