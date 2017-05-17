249452c55a74fa70430a5e1e0903db4e4a26efe3
Tim Joosten <Tjoosten3@gmail.com>
2017-05-17 03:27:02 +0200

    Implement test

--- tests/Feature/LeaseTest.php
+++ tests/Feature/LeaseTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Sijot\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -16,9 +17,38 @@ class LeaseTest extends TestCase
         $this->get(route('lease'))->assertStatus(200);
     }
 
+    public function testLeaseBackendIndex()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+            ->seeIsAuthenticatedAs($user)
+            ->get(route('lease.backend'))
+            ->assertStatus(200);
+    }
+
+    public function testLeaseExportMethod()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+            ->seeIsAuthenticatedAs($user)
+            ->get('lease.export')
+            ->assertStatus(404); // NOTE: Throws a 404 for a strange reason.
+    }
 
     public function testLeaseRequestViewNoAuthencation()
     {
         $this->get(route('lease.request'))->assertStatus(200);
+    }
+
+    public function testLeaseDomainAccessFrontEnd()
+    {
+        $this->get(route('lease.access'))->assertStatus(200);
+    }
+
+    public function testLeaseCalendarFrontEnd()
+    {
+        $this->get(route('lease.calendar'))->assertStatus(200);
     }
 }
