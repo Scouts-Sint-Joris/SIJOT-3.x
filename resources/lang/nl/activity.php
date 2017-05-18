e1beb5fd19e4d218bff434bfb0c7eeff38974939
Tim Joosten <Tjoosten3@gmail.com>
2017-05-19 00:46:23 +0200

    Add empty line (activity translations)

--- resources/lang/nl/activity.php
+++ resources/lang/nl/activity.php
@@ -15,6 +15,6 @@ return [
     'flash-store-success'       => 'De activiteit (:title) is opgeslagen.',
     'flash-status-draft'        => 'De activiteit (:title) is omgezet naar een kladversie.',
     'flash-status-publish'      => 'De activiteit (:title) is omgezet naar een publicatie',
-    'flash-delete-success'      => 'De activiteit (:title) is verwijderd'
+    'flash-delete-success'      => 'De activiteit (:title) is verwijderd',
 
 ];
