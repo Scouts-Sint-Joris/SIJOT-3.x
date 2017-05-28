ab6d4dfffc0232309925cc4dd51f0f2a322b8e91
Tim Joosten <Tjoosten3@gmail.com>
2017-05-28 22:17:18 +0200

    Update

--- tests/Feature/UsersTest.php
+++ tests/Feature/UsersTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Sijot\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -14,22 +15,72 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
  */
 class UsersTest extends TestCase
 {
+    use DatabaseMigrations, DatabaseTransactions;
+
+    /**
+     * Test the index route for the user section.
+     *
+     * @test
+     * @group all
+     */
     public function testUsersIndex()
     {
+        $user = factory(User::class)->create();
 
+        $this->actingAs($user)
+            ->seeIsAuthenticatedAs($user)
+            ->get(route('users.index'))
+            ->assertStatus(200);
     }
 
+    /**
+     * Test if we can get a specific user.
+     *
+     * @test
+     * @group all
+     */
     public function testGetByValidId()
     {
+        $user = factory(User::class)->create();
 
+        $this->actingAs($user)
+            ->seeIsAuthenticatedAs($user)
+            ->get(route('users.getId', $user->id))
+            ->assertStatus(200)
+            ->json();
     }
 
+    /**
+     * Test if we can get an 404 on a invalid user request.
+     *
+     * @test
+     * @group all
+     */
     public function testGetByInvalidId()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+            ->seeIsAuthenticatedAs($user)
+            ->get(route('users.getId', 1000))
+            ->assertStatus(404);
+    }
+
+    /**
+     * Test if we can ban a user. Without validation errors.
+     *
+     * @test
+     * @group all
+     */
+    public function testBanUserValidIdNoValidationError()
     {
 
     }
 
-    public function testBanUserValidId()
+    /**
+     *
+     */
+    public function testBanUserValidIdValidationError()
     {
 
     }
