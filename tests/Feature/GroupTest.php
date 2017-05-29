0f0eeae7621a0746858c71314757558deec8b56c
Tim Joosten <Tjoosten3@gmail.com>
2017-05-29 04:34:45 +0200

    Implement groups tests

--- tests/Feature/GroupTest.php
+++ tests/Feature/GroupTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use Sijot\Groups;
+use Sijot\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
@@ -21,5 +23,105 @@ class GroupTest extends TestCase
     {
         $this->get(route('groups.index'))
             ->assertStatus(200);
+    }
+
+    /**
+     * Test The backend route.
+     *
+     * @test
+     * @group all
+     */
+    public function testBackendIndex()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+            ->seeIsAuthenticatedAs($user)
+            ->get(route('groups.backend'))
+            ->assertStatus(200);
+    }
+
+    /**
+     * Try to update a group (with validation errors)
+     *
+     * @test
+     * @group all
+     */
+    public function testUpdateValidationErrors()
+    {
+        $user  = factory(User::class)->create();
+        $group = factory(Groups::class)->create();
+
+        $this->actingAs($user)
+            ->seeIsAuthenticatedAs($user)
+            ->post(route('groups.update', ['id' => $group->id]))
+            ->assertStatus(200)
+            ->assertSessionHasErrors()
+            ->assertSessionMissing([
+                'class'   => 'alert alert-success',
+                'message' => 'De groeps informatie is aangepast'
+            ]);
+    }
+
+    /**
+     * Try to update a group (without validation errors)
+     *
+     * @test
+     * @group all
+     */
+    public function testUpdateSuccess()
+    {
+        $user  = factory(User::class)->create();
+        $group = factory(Groups::class)->create();
+
+        $old = [
+            'title'       => $group->title,
+            'sub_title'   => $group->sub_title,
+            'description' => $group->description,
+        ];
+
+        $input = [
+            'title'       => 'Ik ben een title',
+            'sub_title'   => 'Ik ben een sub titel',
+            'description' => 'Ik ben een beschrijving',
+        ];
+
+        $this->actingAs($user)
+            ->seeIsAuthenticatedAs($user)
+            ->post(route('groups.update', ['id' => $group->id]), $input)
+            ->assertStatus(302)
+            ->assertSessionHas([
+                'class'   => 'alert alert-success',
+                'message' => 'De groeps informatie is aangepast'
+            ]);
+
+        $this->assertDatabaseHas('groups', $input);
+        $this->assertDatabaseMissing('groups', $old);
+    }
+
+    /**
+     * Try to update a group (no valid id)
+     *
+     * @test
+     * @group all
+     */
+    public function testUpdateNoValidId()
+    {
+        $user  = factory(User::class)->create();
+
+        $input = [
+            'title'       => 'Ik ben een title',
+            'sub_title'   => 'Ik ben een sub titel',
+            'description' => 'Ik ben een beschrijving',
+        ];
+
+        $this->actingAs($user)
+            ->seeIsAuthenticatedAs($user)
+            ->post(route('groups.update', ['id' => 1000]), $input)
+            ->assertStatus(404)
+            ->assertSessionMissing([
+                'class'   => 'alert alert-success',
+                'message' => 'De groeps informatie is aangepast'
+            ]);
     }
 }
