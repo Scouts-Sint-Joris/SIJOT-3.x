29705a27f3faf082d5dcbb8571ebadb38f78343c
Tim Joosten <Tjoosten3@gmail.com>
2017-08-07 15:51:21 +0200

    Register the models

---
+++ app/Permission.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Sijot;
+
+class Permission extends \Spatie\Permission\Models\Permission
+{
+    //
+}
---
+++ app/Role.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Sijot;
+
+
+class Role extends \Spatie\Permission\Models\Role
+{
+    //
+}
