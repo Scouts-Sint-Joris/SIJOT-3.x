f012c5bed79ac90fad2a3e701f7ab3274833311f
Tim Joosten <Tjoosten3@gmail.com>
2017-08-07 16:08:26 +0200

    Implement notes

--- app/Permission.php
+++ app/Permission.php
@@ -4,5 +4,5 @@ namespace Sijot;
 
 class Permission extends \Spatie\Permission\Models\Permission
 {
-    //
+    //? Model is extended for future use when we need custom logic.
 }
--- app/Role.php
+++ app/Role.php
@@ -5,5 +5,5 @@ namespace Sijot;
 
 class Role extends \Spatie\Permission\Models\Role
 {
-    //
+    //? Model is extended for future use when we need custom logic.
 }
