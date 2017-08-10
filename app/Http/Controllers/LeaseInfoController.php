ad774922d6f70765408743475097453d97aa1a6e
Tim Joosten <Tjoosten3@gmail.com>
2017-08-10 02:34:54 +0200

    Start on the feature

---
+++ app/Http/Controllers/LeaseInfoController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Sijot\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Sijot\Http\Requests\LeaseInfoValidator;
+
+class LeaseInfoController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update(LeaseInfoValidator $input, $id)
+    {
+
+    }
+}
