bc268b15ab554bc7df17f6e0b8b17c5c2ee3c830
Tim Joosten <Tjoosten3@gmail.com>
2017-08-13 15:21:57 +0200

    Change backend title

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-        <title>AdminLTE 2 | Blank Page</title>
+        <title>Scout(s en Gidsen Sint-Joris | Backend</title>
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
