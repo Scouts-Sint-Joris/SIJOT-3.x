2a27571dd7e7fc98f002ae43283a8b27d834141a
Tim Joosten <Tjoosten3@gmail.com>
2017-08-25 12:45:21 +0200

    Implement api key package

---
+++ app/Http/Controllers/ApiKeyController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sijot\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiKeyController extends Controller
+{
+    public function __construct()
+    {
+
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function makeKey()
+    {
+
+    }
+
+    public function deleteKey()
+    {
+
+    }
+}
