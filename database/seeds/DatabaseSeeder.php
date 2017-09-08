26300974a986402ea781fd8d3e35cbe71a6cec66
Tim Joosten <Tjoosten3@gmail.com>
2017-09-08 19:42:56 +0200

    Seeder fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,30 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        if (app()->environment() === 'production') {
-            exit();
-        }
-
-        Eloquent::unguard();
-
-        // Truncate all tables, except migrations.
-        $tables      = DB::select('SHOW TABLES');
-        $queryOutput = 'Tables_in_' . getenv('DB_DATABASE');
-
-        foreach($tables as $table) {
-            if ($table->$queryOutput !== 'migrations') {
-                DB::table($table->$queryOutput)->truncate();
-            }
-        }
-
-        // Find and run all seeders
-        $classes = require base_path().'/vendor/composer/autoload_classmap.php';
-
-        foreach ($classes as $class) {
-            if (strpos($class, 'TableSeeder') !== false) {
-                $seederClass = substr(last(explode('/', $class)), 0, -4);
-                $this->call($seederClass);
-            }
-        }
+        $this->call(GroupsSeeder::class);
+        $this->call(RoleTableSeeder::class);
+        $this->call(ThemeTableSeeder::class);
+        $this->call(AbilityTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
     }
 }
